49e5b27f85ab1b216026febb347a8efc9d8e293c
Your Name <you@example.com>
2022-02-09 19:45:18 +0530

    check

--- src/main/kotlin/com/axis/springlearn/controller/Controller.kt
+++ src/main/kotlin/com/axis/springlearn/controller/Controller.kt
@@ -1,5 +1,8 @@
 package com.axis.springlearn.controller
 
+import com.axis.springlearn.model.Student
+import com.axis.springlearn.service.StudentService
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -8,5 +11,9 @@ class Controller {
     @RequestMapping("/hello")
     fun helloworld() : String{
         return "hello"
+    }
+    @GetMapping("/students")
+    fun getStudents(): Student{
+        return StudentService.getAll()
     }
 }
---
+++ src/main/kotlin/com/axis/springlearn/model/Student.kt
@@ -0,0 +1,3 @@
+package com.axis.springlearn.model
+
+class Student(var id: Int,var name: String, var gender: String, var email: String, var pnone: String)
